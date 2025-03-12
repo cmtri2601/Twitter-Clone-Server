b752829cfad5ff0baa7a7eb71a9d2b34dafbda55
minh-tri <cmtri2601@gmail.com>
2025-03-12 16:54:50 +0700

    create single, compound, ttl indexes

--- src/database/index.ts
+++ src/database/index.ts
@@ -26,11 +26,15 @@ class DataBase {
     try {
       // Connect the client to the server	(optional starting in v4.7)
       await this.client.connect();
+
       // Send a ping to confirm a successful connection
       await this.db.command({ ping: 1 });
       console.log(
         'Pinged your deployment. You successfully connected to MongoDB!'
       );
+
+      // Init index
+      await this.initIndexes();
     } catch (error) {
       console.log('Error', error);
       throw error;
@@ -38,24 +42,94 @@ class DataBase {
   }
 
   /**
-   * get users collection
+   * Init index
+   */
+  async initIndexes() {
+    await this.initUserIndexes();
+    await this.initRefreshTokenIndexes();
+    await this.initFollowerIndexes();
+  }
+
+  /**
+   * Get users collection
    */
   get users(): Collection<UserEntity> {
     return this.db.collection('users');
   }
 
   /**
-   * get refresh_token collection
+   * Init indexes of user collection
+   */
+  async initUserIndexes() {
+    // Check indexes are already existed
+    const isExisted = await this.users.indexExists([
+      'email_1',
+      'username_1',
+      'email_1_password_1'
+    ]);
+
+    if (isExisted) {
+      return;
+    }
+
+    // Create indexes
+    await this.users.createIndex({ email: 1 }, { unique: true });
+    await this.users.createIndex({ username: 1 }, { unique: true });
+    await this.users.createIndex({ email: 1, password: 1 });
+  }
+
+  /**
+   * Get refresh_token collection
    */
   get refreshTokens(): Collection<RefreshTokenEntity> {
     return this.db.collection('refresh_tokens');
   }
 
   /**
-   * get followers collection
+   * Init indexes of user collection
+   */
+  async initRefreshTokenIndexes() {
+    // Check indexes are already existed
+    const isExisted = await this.refreshTokens.indexExists([
+      'token_1',
+      'exp_1'
+    ]);
+
+    if (isExisted) {
+      return;
+    }
+
+    // Create indexes
+    await this.refreshTokens.createIndex({ token: 1 }, { unique: true });
+    await this.refreshTokens.createIndex({ exp: 1 }, { expireAfterSeconds: 0 });
+  }
+
+  /**
+   * Get followers collection
    */
   get followers(): Collection<FollowerEntity> {
     return this.db.collection('followers');
+  }
+
+  /**
+   * Init indexes of user collection
+   */
+  async initFollowerIndexes() {
+    // Check indexes are already existed
+    const isExisted = await this.followers.indexExists([
+      'user_id_1',
+      'follower_id_1',
+      'user_id_1_follower_id_1'
+    ]);
+
+    if (isExisted) {
+      return;
+    }
+
+    // Create indexes
+    await this.followers.createIndex({ user_id: 1 });
+    await this.followers.createIndex({ follower_id: 1 });
+    await this.followers.createIndex({ user_id: 1, follower_id: 1 });
   }
 }
 
